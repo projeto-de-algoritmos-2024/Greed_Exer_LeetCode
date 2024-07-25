09759af04d4ef4d38f05c499bbc4b205cad384a4
CarolCoCe <anaipva@gmail.com>
2024-07-25 17:57:37 -0300

    primeira tentativa, caso 1 deu certo

---
+++ Course Schedule/course schedule.py
@@ -0,0 +1,16 @@
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+
+        #ordenar do menor deadline para o maior
+        copiaOrdenada = sorted(courses, key=lambda course : course[1])
+        
+        #Incluir na solucao se for compativel, é possível acabar o curso antes do deadline dele
+        tempoTotal = 0
+        qtdCourses = 0
+        
+        for course in copiaOrdenada:
+           if tempoTotal + course[0] <= course[1]:
+               qtdCourses = qtdCourses + 1
+                   
+        return qtdCourses
+                   
