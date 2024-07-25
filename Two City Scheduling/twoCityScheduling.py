ca2060632792706b25d9859755cf14e49bf17cd2
CarolCoCe <anaipva@gmail.com>
2024-07-24 22:04:38 -0300

    two city- problemas com sintaxe

---
+++ Two City Scheduling/twoCityScheduling.py
@@ -0,0 +1,31 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+
+        #ordenar da maior diferenca de preco para a menor
+        copiaOrdenada = sorted(costs, reverse=True, key=lambda diferenca : abs(itemgetter(0) - itemgetter(1)))
+        
+        #Incluir na solucao se for compativel, ou seja, a e b devem ter a msm qtd de pessoas e sempre devemos optar pelo mais barato
+        n = (len(costs))/2
+        a = 0
+        b = 0
+        totalCost = 0
+        
+        for person in copiaOrdenada:
+           if person[0] > person[1]: #a ta mais barato que b
+               if a <= n:
+                   a = a+1
+                   totalCost = totalCost + person[0]
+               else:
+                   b = b+1
+                   totalCost = totalCost + person[1]
+           else: # b ta mais barato que a
+                if b <= n:
+                   b = b+1
+                   totalCost = totalCost + person[1]
+                else:
+                   a = a+1
+                   totalCost = totalCost + person[0]
+                   
+        return totalCost
+                   
