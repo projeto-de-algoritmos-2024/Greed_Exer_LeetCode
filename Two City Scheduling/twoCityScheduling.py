7d1f9d6abd609c663a04d6f842e37ab1aecae06c
CarolCoCe <anaipva@gmail.com>
2024-07-24 23:13:47 -0300

    Deu tudo certo, era problema de <>= de novo kkkk

--- Two City Scheduling/twoCityScheduling.py
+++ Two City Scheduling/twoCityScheduling.py
@@ -3,7 +3,7 @@ class Solution:
         
 
         #ordenar da maior diferenca de preco para a menor
-        copiaOrdenada = sorted(costs, key=lambda person : abs(person[0] - person[1]))
+        copiaOrdenada = sorted(costs, reverse= True, key=lambda person : abs(person[0] - person[1]))
         
         #Incluir na solucao se for compativel, ou seja, a e b devem ter a msm qtd de pessoas e sempre devemos optar pelo mais barato
         n = (len(costs))/2
@@ -13,14 +13,14 @@ class Solution:
         
         for person in copiaOrdenada:
            if person[0] < person[1]: #a ta mais barato que b
-               if a <= n:
+               if a < n:
                    a = a+1
                    totalCost = totalCost + person[0]
                else:
                    b = b+1
                    totalCost = totalCost + person[1]
            else: # b ta mais barato que a
-                if b <= n:
+                if b < n:
                    b = b+1
                    totalCost = totalCost + person[1]
                 else:
