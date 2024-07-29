6db88e8a39cea1a68bf76f2cde7a40ce17d3d956
CarolCoCe <anaipva@gmail.com>
2024-07-29 18:06:01 -0300

    Segundo exercicio dificil ok, ja coloquei as imagens

--- Course Schedule/course schedule.py
+++ Course Schedule/course schedule.py
@@ -2,16 +2,24 @@ class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
 
         #ordenar do menor deadline para o maior
-        copiaOrdenada = sorted(courses, key=lambda course: (course[1], course[0]))
+        courses.sort(key=lambda co: co[1])
+        #trocar sorted por sort pq nao vamos ligar pra boas praticas agr, vamos pela memoria
         
         #Incluir na solucao se for compativel, é possível acabar o curso antes do deadline dele
-        tempoTotal = 0
-        qtdCourses = 0
+        #Usar um max heap para manter o curso de maior duracao em cima e ir substituindo
         
-        for course in copiaOrdenada:
-           if tempoTotal + course[0] <= course[1]:
-               qtdCourses = qtdCourses + 1
-               tempoTotal = tempoTotal + course[0]
+        heap = []
+        tempoTotal = 0
+        
+        for duration, deadline in courses:
+           if tempoTotal + duration <= deadline: 
+               tempoTotal += duration
+               heapq.heappush(heap, -duration) #inclui na nossa solucao (heap) e coloca negativo para a maior
+               #                                duracao ficar em cima, já que é um max heap
+               
+           elif heap and -heap[0] > duration:
+               tempoTotal += duration + heap[0] #como ja ta negativo, so soma
+               heapq.heapreplace(heap, -duration)
                    
-        return qtdCourses
+        return len(heap)
                    
