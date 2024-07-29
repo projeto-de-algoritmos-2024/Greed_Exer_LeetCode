72db9d069009ebd6b6a52924e971b6ba3e7060fc
CarolCoCe <anaipva@gmail.com>
2024-07-29 14:19:41 -0300

    segundo medio completo, era so arrumar o intervalo do fim e o criterio de ordenacao

--- non-overlapping/nonOverlapping.py
+++ non-overlapping/nonOverlapping.py
@@ -1,11 +1,11 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
          #ordenar do menor fim para o maior
-        copiaOrdenada = sorted(intervals, key=lambda inter: (inter[0]))
+        copiaOrdenada = sorted(intervals, key=lambda inter: (inter[1]))
         
         #Incluir na solucao se for compativel
         intervalosCompativeis = 0
-        fim = 0
+        fim = -50000
         
         for inter in copiaOrdenada:
            if inter[0] >= fim: # é compativel pq comeca depois do fim do ultimo
